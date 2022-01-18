1783506994c977ffd73046dae0254e67d98989db
mrsakatora <97986513+mrsakatora@users.noreply.github.com>
2022-01-19 04:20:42 +0800

    Add files via upload

---
+++ initial-debian.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Script for server initial setup
+
+clear
+echo "Check for new update:"
+sleep 2
+apt update
+apt list --upgradable
+sleep 2
+apt upgrade -y
+
+clear
+echo "Change local timezone:"
+sleep 2
+ln -sf /usr/share/zoneinfo/Asia/Singapore /etc/localtime
+apt purge ntp -y
+systemctl start systemd-timesyncd
+timedatectl
+sleep 2
+
+clear
+echo "Install speedtest.net:"
+sleep 2
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | bash
+apt install speedtest -y
+
+clear
+echo "Install BBRplus+FQ:"
+sleep 2
+wget https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcp.sh && chmod +x tcp.sh && ./tcp.sh
+./tcp.sh
+reboot
